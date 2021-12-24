2dfde47ad0c5899a91660ac538a8e28456cf12cf
SebLeyr <sebastien.leyrissoux@gmail.com>
2021-12-24 15:34:52 +0100

    ajout animation ouverture modal, essai animation fermeture

--- Réalisation_2_HTML CSS/CreerCompte.js
+++ Réalisation_2_HTML CSS/CreerCompte.js
@@ -5,23 +5,24 @@ var crea = document.getElementById("creaCompte");
 var open = document.getElementById("creaCpt");
 
 //récupérer le bouton de fermeture de la page
-const iWindow = crea.contentWindow;
-const iDocument = iWindow.document;
-var close = iDocument.getElementById("close");
+var close = document.getElementById("close");
 
 //ouverture de la page avec le bouton
 open.addEventListener("click", function() {
-    crea.style.display = "block";
+    crea.style.display = "flex";
+    crea.style.animation = "slideTop 0.5s";
 })
 
 //fermeture de la page avec le bouton
 close.addEventListener("click", function() {
+    crea.style.animation = "slideOut 0.5s";
     crea.style.display = "none";
 })
 
 //fermeture de la page avec le background
 window.addEventListener("click", function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == crea) {
+        crea.style.animation = "slideOut 0.5s";
+        crea.style.display = "none";
     }
 })
